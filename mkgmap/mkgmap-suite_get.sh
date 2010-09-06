77c118ba9d50392b2c01043bbf84409229a952a1
patricks <patrick@helsmdeep.at>
2010-09-06 11:45:14 +0200

    added some basic mkgmap downloading script

---
+++ mkgmap/mkgmap-suite_get.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+###############################################################################
+#                                                                             #
+# download the latest mkgmap svn version                                      #
+#                                                                             #
+# @version: 1.00                                                              #
+# @author: Steiner Patrick <patrick@helmsdeep.at>                             #
+# @date: 06.09.2010 11:21                                                     #
+# License: GPL                                                                #
+# http://www.fsf.org/licenses/gpl.htmlfree for all                            #
+#                                                                             #
+###############################################################################
+
+CWD=`pwd`
+
+svn co http://svn.mkgmap.org.uk/splitter/trunk splitter-svn
+
+svn co http://svn.parabola.me.uk/mkgmap/trunk mkgmap-svn
+
+cd $CWD/splitter-svn
+ant
+
+cd $CWD/mkgmap-svn
+ant
+
