8241fd08237df519dcd83b012f4828d71803483e
patrick <patrick@helmsdeep.at>
2011-01-10 16:17:03 +0100

    renamed mkgmap download script fixed a bug in the option.args file

--- mkgmap/osm-mkgmap-suite_get.sh
+++ mkgmap/osm-mkgmap-suite_get.sh
@@ -18,10 +18,13 @@ fi
 
 CWD=`pwd`
 
+rm -rf splitter*
+rm -rf mkgmap*
+
 curl -O http://www.mkgmap.org.uk/splitter/splitter-r123.tar.gz
 tar -xvzf splitter-r123.tar.gz
 ln -s splitter-r123/splitter.jar splitter.jar
 
 curl -O http://www.mkgmap.org.uk/snapshots/mkgmap-latest.tar.gz
 tar -xvzf mkgmap-latest.tar.gz
-ln -s mkgmap-r1670/mkgmap.jar mkgmap.jar
+ln -s mkgmap-r1773/mkgmap.jar mkgmap.jar
